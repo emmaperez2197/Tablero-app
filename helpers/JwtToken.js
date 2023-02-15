08c7c2737c8be7d506625977df38234c8de177f0
Emmanuel <eperez@moodtechnology.com.ar>
2023-02-14 22:55:15 -0300

    changes file herlpers and register

--- helpers/JwtToken.js
+++ helpers/JwtToken.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 class Token {
 
     static sing(data){
-       return jwt.sign(data, process.env.KEY_PRELOGIN, { algorithm: 'RS256' });
+       return jwt.sign(data, process.env.KEY_PRELOGIN);
     }
 
     static decode(token){
--- src/routes/user/src/register.js
+++ src/routes/user/src/register.js
@@ -1,7 +1,8 @@
 const { Router } = require('express');
-const User = require('../../../models/User')
-const {sing} = require('../../../../helpers/JwtToken')
-const validate = require('../../../structures/user/register')
+const User = require('../../../models/User');
+const {sing} = require('../../../../helpers/JwtToken');
+const {messageForExistantEmail} = require('../../../messages/user/register');
+const validate = require('../../../structures/user/register');
 
 
 const app = Router();
@@ -16,9 +17,23 @@ const handler = async (req, res) => {
 		return res.status(400).json(validateUser)
 	}
 
-	checkEmailExists = User.getOne({email})
+	checkEmailExists = await User.get({email:email})
 
-	res.json(checkEmailExists)
+	if (checkEmailExists.length) {
+		return res.status(200).json(messageForExistantEmail())
+	}
+
+
+	console.log('pase');
+	const data = {
+		nombre, 
+		apellido,
+		email,
+		contraseña
+	}
+
+	const token = sing(data)
+	console.log(token);
 
 };
 
