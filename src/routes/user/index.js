2d87e4d2d17586e231d901bddff9f3621e6381d6
Emmanuel <eperez@moodtechnology.com.ar>
2023-02-17 16:27:20 -0300

    Changes routes

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -2,8 +2,11 @@ const nameModule = 'user/';
 
 const { app: register } = require('./src/register');
 const { app: confirmRegister } = require('./src/confirmRegister.');
+const { app: login } = require('./src/login');
 
 module.exports = define => {
 	define(nameModule + 'register', register);
 	define(nameModule + 'confirmRegister', confirmRegister);
+	define(nameModule + 'login', login);
+
 };
