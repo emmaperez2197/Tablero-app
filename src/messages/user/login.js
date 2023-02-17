2dfc2fafb2ebbff3d3e3755a6df4414c2623924a
Emmanuel <eperez@moodtechnology.com.ar>
2023-02-17 15:08:10 -0300

    Add message login

---
+++ src/messages/user/login.js
@@ -0,0 +1,7 @@
+const mensajes = {
+    passwordInvalid: 'Contraseña incorrecta intente nuevamente',
+    successfulLogin: (nombre)=>`Bienvenido ${nombre}, se ha logueado exitosamente`
+}
+
+module.exports = {mensajes}
+
