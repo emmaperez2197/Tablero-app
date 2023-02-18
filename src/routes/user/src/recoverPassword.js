c244981921996274e22cd72a65efbb33bfecd430
Emmanuel <eperez@moodtechnology.com.ar>
2023-02-17 23:13:44 -0300

    Add template recoverPass and validation

--- src/routes/user/src/recoverPassword.js
+++ src/routes/user/src/recoverPassword.js
@@ -3,13 +3,28 @@ const {Router} = require('express');
 const UserModel = require('../../../models/User');
 const Token = require('../../../../helpers/JwtToken.js');
 const Bcrypt = require('../../../../helpers/bcrypt');
+const validateSchema = require('../../../structures/user/recoverPass')
 
 
 const app = Router();
 
 const handler = async (req, res)=>{
-    console.log('hola');
-    
+
+    const validateBody = await validateSchema(req.body);
+
+    if (validateBody.error) {
+        return res.status(400).json(validateBody)
+    }
+    try {
+        const checkExistsEmail = await UserModel.get({email: req.body.email})
+
+        if (!checkExistsEmail.length) {
+            
+        }
+
+    } catch (error) {
+        return res.json(500).json({error: error.toString()})
+    }
 }
 
 
