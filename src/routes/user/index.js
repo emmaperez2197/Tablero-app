65d7ca88fd5f393f0ba9565927e742934664a2c2
Emmanuel <eperez@moodtechnology.com.ar>
2023-02-18 00:01:04 -0300

    Add route for confirm password

--- src/routes/user/index.js
+++ src/routes/user/index.js
@@ -4,10 +4,13 @@ const { app: register } = require('./src/register');
 const { app: confirmRegister } = require('./src/confirmRegister.');
 const { app: login } = require('./src/login');
 const { app: recoverPassword } = require('./src/recoverPassword');
+const { app: confirmPassword } = require('./src/ConfirmPassword');
 
 module.exports = define => {
 	define(nameModule + 'register', register);
 	define(nameModule + 'confirmRegister', confirmRegister);
 	define(nameModule + 'login', login);
 	define(nameModule + 'recoverPassword', recoverPassword);
+	define(nameModule + 'confirmPassword', confirmPassword);
+
 };
