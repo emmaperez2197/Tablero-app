a02d7fccb97d91d23533371951d913e22dbfa8e1
Emmanuel <eperez@moodtechnology.com.ar>
2023-02-23 17:03:47 -0300

    test finished

--- src/routes/user/src/ConfirmPassword.js
+++ src/routes/user/src/ConfirmPassword.js
@@ -17,34 +17,27 @@ const handler = async (req, res)=>{
     if (validateBody.error) {
         return res.status(400).json(validateBody)
     }
+
     try {
         const {nuevaContraseña }= req.body
 
         const decodeData = await Token.decode(req.params.token);
 
-        console.log('decodeDAtaaaaaa',decodeData);
-
         const getUser = await UserModel.getOne({email: decodeData});
-
-        console.log('getUSeeeer', getUser);
-
         
-        const {_id} = getUser
-
-        const id = _id.toString();
-
         if (!getUser) {
-            return res.status(404).json({message:messageForNonExistantEmail()})
+            return res.status(404).json({message: messageForNonExistantEmail()})
         }
+        
+         const {_id} = getUser;
+
+         const id = _id.toString();
 
         const hashNewPassword = await Bcrypt.hashUser(nuevaContraseña);
 
-        console.log('nuevopassword', hashNewPassword);
-
-        console.log(        await UserModel.findOneAndModify(id, {contraseña: hashNewPassword}));
-        // await UserModel.findOneAndModify(id, {contraseña: hashNewPassword})
+        await UserModel.findOneAndModify(id, {contraseña: hashNewPassword})
         
-        res.status(200).json({message: message.recoverPassConfirmed, code: 2})
+        res.status(200).json({message: message.recoverPassConfirmed, code: 2});
     } catch (error) {
         return res.status(500).json({error: error.toString()})
     }
--- tests/user/confirmPassword.test.js
+++ tests/user/confirmPassword.test.js
@@ -4,20 +4,17 @@ const assert = require('assert');
 const Token = require('../../helpers/JwtToken');
 
 const { mockRequest, mockResponse } = require('../../tests/mocks/index');
-const {handler} = require('../../src/routes/user/src/recoverPassword');
+const { handler } = require('../../src/routes/user/src/ConfirmPassword');
 const UserModel = require('../../src/models/User')
-const message = require('../../src/messages/user/recoverPass');
-const EmailService = require('../../services/EmailService');
 const Bcrypt = require('../../helpers/bcrypt');
-const { token } = require('morgan');
+const message = require('../../src/messages/user/recoverPass');
+const {messageForNonExistantEmail} = require('../../src/messages/user/register');
 
 describe('Create confirm password api test', ()=>{
 
     afterEach(() => sandbox.restore());
     beforeEach(() => { process.env.KEY_PRELOGIN = true});
 
-
-      
     const decodeData =  "eperez@moodtechnology.com.ar"
       
     const getUser = {
@@ -30,10 +27,7 @@ describe('Create confirm password api test', ()=>{
        
     const hash = '$2b$12$ym5qhZZ4PQewUxAlqP8XdemtftvFwr9GpE9Bw84e1y2yjYtDCzxHS'
  
-
     const body = { nuevaContraseña: "123456789"}
-
-
 
     const userUpdated = {
       lastErrorObject: { n: 1, updatedExisting: true },
@@ -51,10 +45,10 @@ describe('Create confirm password api test', ()=>{
 
 
     context('When errors do not ocurrs', () => {
-        it.only('Should return 200 if the user is successfully confirmed', async () => {
+        it('Should return 200 if the user is successfully confirmed', async () => {
 
             sandbox.stub(Token, 'decode').resolves(decodeData);
-            sandbox.stub(UserModel, 'get').resolves(getUser);
+            sandbox.stub(UserModel, 'getOne').resolves(getUser);
             sandbox.stub(Bcrypt, 'hashUser').resolves(hash);
             sandbox.stub(UserModel, 'findOneAndModify').resolves(userUpdated)
 
@@ -65,26 +59,104 @@ describe('Create confirm password api test', ()=>{
 
             await handler(req, res)
 
-            assert.deepStrictEqual(res.status, {});
-            // assert.deepStrictEqual(res.json, {message: message.messageSendEmail(getData[0].email), code:2})
 
-            // sandbox.assert.alwaysCalledWithExactly(UserModel.get, { email: 'eperez@moodtechnology.com.ar' });
-            // sandbox.assert.alwaysCalledWithExactly(Token.sing, "eperez@moodtechnology.com.ar"  )
+            assert.deepStrictEqual(res.status, 200);
+            assert.deepStrictEqual(res.json, {message: message.recoverPassConfirmed, code: 2})
+
+            sandbox.assert.calledOnceWithExactly(Token.decode, undefined)
+            sandbox.assert.calledOnceWithExactly(UserModel.getOne , { email: 'eperez@moodtechnology.com.ar' });
+            sandbox.assert.calledOnceWithExactly(Bcrypt.hashUser, body.nuevaContraseña);
+            sandbox.assert.calledOnceWithExactly(UserModel.findOneAndModify, getUser._id, {contraseña: hash} );
 
         });
-
-
 
     })
 
     context('When an errors ocurrs', () => {
 
+            it('Should return 400 if data invalid', async () => {
+            
+              sandbox.stub(Token, 'decode');
+              sandbox.stub(UserModel, 'getOne');
+              sandbox.stub(Bcrypt, 'hashUser');
+              sandbox.stub(UserModel, 'findOneAndModify');
+            
+            
+            
+              const req = mockRequest({...body, nuevaContraseña:5}, token);
+              const res = mockResponse();
+            
+              await handler(req, res)
+            
+            
+              assert.deepStrictEqual(res.status, 400);
+              assert.deepStrictEqual(res.json, {error: "\"nuevaContraseña\" must be a string"})
+            
+              sandbox.assert.notCalled(Token.decode)
+              sandbox.assert.notCalled(UserModel.getOne);
+              sandbox.assert.notCalled(Bcrypt.hashUser);
+              sandbox.assert.notCalled(UserModel.findOneAndModify );
+
+           });
+
+
+
+
+
+          it('Should return 404 user no exits', async () => {
+
+            sandbox.stub(Token, 'decode').resolves(decodeData);
+            sandbox.stub(UserModel, 'getOne').resolves(null)
+            sandbox.stub(Bcrypt, 'hashUser');
+            sandbox.stub(UserModel, 'findOneAndModify');
+
+
+
+            const req = mockRequest(body, token);
+            console.log(req);
+            const res = mockResponse();
+
+            await handler(req, res);
+
+            assert.deepStrictEqual(res.status, 404);
+            assert.deepStrictEqual(res.json, {message: messageForNonExistantEmail()})
+
+            sandbox.assert.calledOnceWithExactly(Token.decode, undefined)
+            sandbox.assert.calledOnceWithExactly(UserModel.getOne,{ email: 'eperez@moodtechnology.com.ar' } );
+            sandbox.assert.notCalled(Bcrypt.hashUser);
+            sandbox.assert.notCalled(UserModel.findOneAndModify );
+
+        });
      
     
     })
 
     context('When database errors occur', () => {
 
+          it.only('Should return 500 if an error occurs while querying the database', async () => {
+
+            sandbox.stub(Token, 'decode').resolves(decodeData)
+            sandbox.stub(UserModel, 'getOne').rejects(new Error('error in getOne'));
+            sandbox.stub(Bcrypt, 'hashUser');
+            sandbox.stub(UserModel, 'findOneAndModify');
+
+
+
+            const req = mockRequest(body, token);
+            const res = mockResponse();
+
+            await handler(req, res)
+
+
+            assert.deepStrictEqual(res.status, 500);
+            assert.deepStrictEqual(res.json, {error: "Error: error in getOne"})
+
+            sandbox.assert.calledOnceWithExactly(Token.decode, undefined)
+            sandbox.assert.calledOnceWithExactly(UserModel.getOne , { email: 'eperez@moodtechnology.com.ar' });
+            sandbox.assert.notCalled(Bcrypt.hashUser);
+            sandbox.assert.notCalled(UserModel.findOneAndModify);
+
+          });
 
     })
 
