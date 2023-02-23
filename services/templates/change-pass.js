7525b82dab669c4d1dcd3e395820f59864c66f18
Emmanuel <eperez@moodtechnology.com.ar>
2023-02-23 19:39:07 -0300

    Add template changes password

---
+++ services/templates/change-pass.js
@@ -0,0 +1,34 @@
+/* eslint-disable */
+const exchangeNews = () => `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap">
+</head>
+
+<body>
+    <table style="font-size: 20px; width: 500px;font-family: Quicksand, Helvetica, Arial, sans-serif;">
+        <tr>
+            <td>
+                <center>
+                    <img src="https://localhost:777/img/logo.png" style="width: 300px" alt="logo-memo">
+                </center>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <center>
+                    <p>Se ha cambiando correctamente su contreseña</p><br/>
+                </center>
+            </td>
+        </tr>
+    </table>
+
+</body>
+
+</html>;`
+
+module.exports = exchangeNews;
