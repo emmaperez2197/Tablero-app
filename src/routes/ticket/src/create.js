1562f01d61829db34f3def9024a5c0acd840a9e7
Emmanuel <eperez@moodtechnology.com.ar>
2023-02-25 13:27:11 -0300

    Fix enpoind create ticket

--- src/routes/ticket/src/create.js
+++ src/routes/ticket/src/create.js
@@ -1,22 +1,32 @@
 const { Router } = require('express');
 
-const TicketModel = require('../../../models/Ticket')
-const validateSchema = require('../../../structures/ticket/create')
+const TicketModel = require('../../../models/Ticket');
+const UserModel = require('../../../models/User');
+const ColumModel = require('../../../models/Colum');
+
 const messages = require('../../../messages/ticket/create')
-const {validateToken} = require('../../../middlewares/auth-user');
+const { validateTicket } = require('../../../middlewares/validateTicket');
+const { validateToken } = require('../../../middlewares/auth-user');
 
 const app = Router();
 
-const handler = async(req, res) => {
-
-    const validate = await validateSchema(req.body);
-
-    if (validate.error) {
-        return res.status(400).json(validate)
-    }
+const handler = async (req, res) => {
 
     try {
-        
+
+
+        const {_id} = req.jwt
+
+        const  [user, colum ] = await Promise.all([UserModel.findById(_id), ColumModel.findById(req.body.idColum)]) 
+
+        if (!user) {
+            return res.status(404).json({message: messages.informerNoexist, code:1 }) 
+        }
+
+        if (!colum) {
+            return res.status(404).json({message: messages.informerNoexist, code:1 }) 
+        }
+
         const createTicket = new TicketModel(req.body)
 
         await createTicket.create()
@@ -30,6 +40,6 @@ const handler = async(req, res) => {
 
 };
 
-app.post('/',validateToken, handler);
+app.post('/',validateToken, validateTicket,  handler);
 
 module.exports = {app, handler}
