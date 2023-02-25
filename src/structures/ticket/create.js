ddc99d586dc8041b4785a6ec9f68585372b513aa
Emmanuel <eperez@moodtechnology.com.ar>
2023-02-25 02:55:38 -0300

    Add informer and assigned to create joi of ticket

--- src/structures/ticket/create.js
+++ src/structures/ticket/create.js
@@ -1,5 +1,7 @@
 
 const Joi = require('joi')
+Joi.objectId = require('joi-objectid')(Joi);
+
 
 const validateSchema = require('../validateSchema');
 
@@ -7,8 +9,9 @@ module.exports = body => {
     const schema = Joi.object({
         titulo: Joi.string().required(),
         descripcion: Joi.string().required(),
-        idUser: Joi.string().required(),
-        idColum: Joi.string().required()
+        informer: Joi.objectId().required(),
+        assigned: Joi.objectId(),
+        idColum: Joi.objectId().required(),
     })
 
    return  validateSchema(schema, body) 
