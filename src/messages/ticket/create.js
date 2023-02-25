d228b6895a89f11bb75ba8762d4323c08e148c45
Emmanuel <eperez@moodtechnology.com.ar>
2023-02-25 13:31:29 -0300

    Add new messages in ticket

--- src/messages/ticket/create.js
+++ src/messages/ticket/create.js
@@ -1,7 +1,8 @@
 const messages = {
     ticketCreated : "Has creado un nuevo ticket",
     ticketNoFount: 'El ticket no existe',
-    informerNoexist: 'El informador no existe'
+    informerNoexist: 'El informador no existe',
+    ticketsNoExists: 'No hay tickets creados'
 }
 
 module.exports = messages;
