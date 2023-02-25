42189b7e93f050362f41f866d168342ddd905253
Emmanuel <eperez@moodtechnology.com.ar>
2023-02-25 02:49:33 -0300

    Add messages

--- src/messages/Colum/create.js
+++ src/messages/Colum/create.js
@@ -1,5 +1,6 @@
 const messages = {
-    createColum: (nombre)=> `Has creado la columna con el nombre ${nombre}`
+    createColum: (nombre)=> `Has creado la columna con el nombre ${nombre}`,
+    columNoFount: 'La columna no existe'
 }
 
 module.exports = messages
--- src/messages/ticket/create.js
+++ src/messages/ticket/create.js
@@ -1,5 +1,6 @@
 const messages = {
-    ticketCreated : "Has creado un nuevo ticket"
+    ticketCreated : "Has creado un nuevo ticket",
+    ticketNoFount: 'El ticket no existe'
 }
 
 module.exports = messages;
