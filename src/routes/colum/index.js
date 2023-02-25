1baf1e5475cfe3a07aa2ea95f3264773a4a8e7de
Emmanuel <eperez@moodtechnology.com.ar>
2023-02-25 02:51:36 -0300

    Add new route to get tickets

--- src/routes/colum/index.js
+++ src/routes/colum/index.js
@@ -2,7 +2,10 @@ const nameModule = 'colum/';
 
 const { app: create } = require('./src/create');
 
+const { app: addTicket } = require('./src/addTicket');
+
 module.exports = define => {
-	define(nameModule , create);
+	define(nameModule + 'create/', create);
+	define(nameModule + 'addTicket/' , addTicket);
 
 };
